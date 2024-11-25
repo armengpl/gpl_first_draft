2a5aead5fc4d8a4469ae7a26077fa914ab9c7cf8
armengpl <166069746+armengpl@users.noreply.github.com>
2024-11-25 10:18:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Choo Choo": "Welcome to your Flask app - GPL FIRST DRAFT🚅"})
 
 
 if __name__ == '__main__':
